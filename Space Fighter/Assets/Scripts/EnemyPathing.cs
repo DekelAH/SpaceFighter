656b7ed0482edf3cab567d006ff45d02041db3de
dekelPC <dekel16791@gmail.com>
2021-06-30 17:30:13 +0300

    Update 1.1
    * Adding boundries to the game.
    * Making the player shooting.
    * Adding shredder to destroy player projectiles on trigger.
    * Adding way points to the enemy to spawn on first one and move to the others.

---
+++ Space Fighter/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> wayPoints; // List of the enemies positions
+    [SerializeField] float moveSpeed = 2f; // Speed of the enemy
+
+    private int wayPointIndex = 0; // Start way point
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = wayPoints[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveByWayPoints();
+    }
+
+    private void MoveByWayPoints()
+    {
+        if (wayPointIndex <= wayPoints.Count - 1) // Checking if it gone higher than the list length
+        {
+            // Getting the position of the current way point
+            var targetPosition = wayPoints[wayPointIndex].transform.position;
+
+            // Setting the movement speed and giving it frame independent
+            float movementThisFrame = moveSpeed * Time.deltaTime;
+
+            // Setting the target of the next way point
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            // If the enemy reached the target increment the way point index by 1
+            if (transform.position == targetPosition)
+            {
+                wayPointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Space Fighter/Assets/Scripts/Player.cs
+++ Space Fighter/Assets/Scripts/Player.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Configuration parameters
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 1f;
+    [SerializeField] float projectileSpeed = 0f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    [SerializeField] GameObject laserPrefab; // Copy of the player laser game object
+
+    Coroutine firingCoroutine; // variable to implement the coroutine and to stop it when needed
 
     private float xMin, xMax; // X axis boundries variables
     private float yMin, yMax; // Y axis boundries variables
+
 
 
     // Start is called before the first frame update
@@ -18,31 +26,41 @@ public class Player : MonoBehaviour
         SetUpMoveBoundaries();
     }
 
-
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
     }
 
-    // Setting up boundries for the player movement in game
-    private void SetUpMoveBoundaries()
+    private void Fire()
     {
-        // Implementing the main camera in the scene to this variable
-        Camera gameCamera = Camera.main;
-        
-        // Setting the minimun X position of the world space value of the X axis +
-        // padding - to prevent from the player sprite to be half out side of the view because of the pivot point
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        // While hold the space key the coroutine - FireContinuously() starts
+        if (Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
 
-        // Setting the maximun X position of the world space value of the X axis + padding (as mentioned)
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        // When leaving the space key the coroutine stops
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
 
-        // Setting the minimum Y position of the world space value of the Y axis + padding (as mentioned)
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+    private IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            // Creating the player laser from player position and using the default rotation and setting it as game object
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
 
-        // Setting the maximum Y position of the world space value of the Y axis + padding (as mentioned)
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+            // Getting the rigidbody2D component and setting the velocity to the laser at the Y axis
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+
+            // Setting the rate of fire (wait X secs between projectiles)
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
     }
 
     // Setting up the movement of the player
@@ -64,5 +82,25 @@ public class Player : MonoBehaviour
 
         // Setting the new position of the player in game by this mentioned variables
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    // Setting up boundries for the player movement in game
+    private void SetUpMoveBoundaries()
+    {
+        // Implementing the main camera in the scene to this variable
+        Camera gameCamera = Camera.main;
+
+        // Setting the minimun X position of the world space value of the X axis +
+        // padding - to prevent from the player sprite to be half out side of the view because of the pivot point
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+
+        // Setting the maximun X position of the world space value of the X axis + padding (as mentioned)
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        // Setting the minimum Y position of the world space value of the Y axis + padding (as mentioned)
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+
+        // Setting the maximum Y position of the world space value of the Y axis + padding (as mentioned)
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 }
